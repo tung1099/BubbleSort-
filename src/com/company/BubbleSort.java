3fba040f41778fbb63ab5787ece1343bbb2ffe12
Tran Duy Tung <dtroyal1102@gmail.com>
2022-02-24 10:10:08 +0700

    Bubble Sort

---
+++ src/com/company/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class BubbleSort {
+    static int[] list = {5,34,23,45,-89,0,-11,6,2};
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + "\t");
+        }
+    }
+}
+
